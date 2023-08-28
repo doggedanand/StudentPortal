7d1ed14079127bcc1f5f13866b6c71482c4c9003
anand <rd.anandv@gmail.com>
2023-08-28 07:32:08 +0530

    navbar and hompage added

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -1,7 +1,26 @@
 // It will apply on every page, means navbar will show in every page
-const nav = document.querySelector('.navbar')
-fetch('/navbar.html')
-    .then(res => res.text())
-    .then(data => {
-        nav.innerHTML = data
-    });
+const nav = document.querySelector(".navbar");
+fetch("/navbar.html")
+  .then((res) => res.text())
+  .then((data) => {
+    nav.outerHTML = data;
+  });
+// ----------------*******************************------------------------//
+//  For showing nav link on small screen
+//------------------******************************-------------------------//
+/*
+
+
+const navbarToggler = document.querySelector(".navbar-toggler");
+const ul = document.querySelector(".navbar-collapse");
+
+navbarToggler.addEventListener("click", function () {
+  ul.classList.toggle("show");
+});
+*/
+
+
+
+
+// For Active Nav link
+
