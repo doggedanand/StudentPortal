0533442064f43b9c300e9c6f1f9080b8870a69b9
anand <rd.anandv@gmail.com>
2023-09-01 21:22:38 +0530

    dynamic card added on homepage

--- js/about-organization.js
+++ js/about-organization.js
@@ -1,5 +1,4 @@
 setTimeout(() => {
- 
   fetch("/db/about-organization.json")
     .then((response) => response.json())
     .then((data) => {
@@ -7,25 +6,55 @@ setTimeout(() => {
 
       data.forEach((item) => {
         const cardDiv = document.createElement("div");
-        cardDiv.className = "col-md-12 mb-3";
-
-        cardDiv.innerHTML = `
-            <div class="card" style="width: 18rem">
-              <div style="display: flex; flex-direction: row">
-                <img
-                  src="${item.img}"
-                  class="card-img-top"
-                  alt="..."
-                  style="flex: 1; margin-right: 600px"
-                />
-                <div class="card-body" style="flex: 2">
-                  <h5 class="card-title">${item.heading}</h5>
-                  <p class="card-text">${item.content}</p>
-                
-                </div>
+        console.log("cardDiv ", cardDiv);
+        cardDiv.className = " col-md-12  mb-3";
+        if (item.row == "first") {
+          cardDiv.innerHTML = `
+          <div class="card mb-3 text-center" style="max-width: 1141px">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <img
+                src="${item.img}"
+                class="img-fluid rounded-start"
+                alt="..."
+                style="margin-right: 20px"
+              />
+            </div>
+            <div class="col-md-6">
+              <div class="card-body">
+                <h5 class="card-title">${item.heading}</h5>
+                <p class="card-text">
+                  ${item.content}
+                </p>
               </div>
             </div>
-          `;
+          </div>
+        </div>
+            `;
+        } else {
+          cardDiv.innerHTML = `
+          <div class="card mb-3 text-center" style="max-width: 1141px">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <div class="card-body">
+                <h5 class="card-title">${item.heading}</h5>
+                <p class="card-text">
+                  ${item.content}
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <img
+                src="${item.img}"
+                class="img-fluid rounded-start"
+                alt="..."
+                style="margin-right: 20px"
+              />
+            </div>
+          </div>
+        </div>
+            `;
+        }
 
         cardContainer.appendChild(cardDiv);
       });
@@ -33,5 +62,4 @@ setTimeout(() => {
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
-
 }, 50);
--- js/loadNotification.js
+++ js/loadNotification.js
@@ -21,17 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
 // For scroll notification bar
 
 setTimeout(() => {
-  // Get a reference to the notification bar container
   const notificationBarContainer = document.querySelector(".card-body");
 
-  // Define the scroll speed (adjust as needed)
-  const scrollSpeed = 1; // Increase for slower scrolling, decrease for faster scrolling
+  const scrollSpeed = 1;
 
-  // Function to scroll the notification bar content
   function scrollNotificationBar() {
     notificationBarContainer.scrollTop += scrollSpeed;
 
-    // Reset the scroll position when it reaches the end
     if (
       notificationBarContainer.scrollTop >=
       notificationBarContainer.scrollHeight -
@@ -41,15 +37,12 @@ setTimeout(() => {
     }
   }
 
-  // Set the scrolling interval
-  let scrollInterval = setInterval(scrollNotificationBar, 50); // Adjust the interval as needed
+  let scrollInterval = setInterval(scrollNotificationBar, 50);
 
-  // Pause scrolling when the mouse hovers over the notification bar
   notificationBarContainer.addEventListener("mouseenter", () => {
     clearInterval(scrollInterval);
   });
 
-  // Resume scrolling when the mouse leaves the notification bar
   notificationBarContainer.addEventListener("mouseleave", () => {
     scrollInterval = setInterval(scrollNotificationBar, 50);
   });
