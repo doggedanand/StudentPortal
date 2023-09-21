5963122159ac8fb66027156589318e8fc3744a19
anand <rd.anandv@gmail.com>
2023-09-21 22:21:52 +0530

    some issue fixed of student home page

--- js/register.js
+++ js/register.js
@@ -1,3 +1,5 @@
+/*
+
 document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("registerButton")
@@ -22,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let userData = [];
 
       if (userDataJSON) {
-        userData = JSON.parse(userDataJSON);
+        userData.push(JSON.parse(userDataJSON));
       }
 
       // Generate a unique ID for the new user
@@ -56,3 +58,73 @@ document.addEventListener("DOMContentLoaded", function () {
 function generateUniqueId() {
   return Math.floor(Math.random() * 90000) + 10000;
 }
+
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("registerButton")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const firstName = document.getElementById("firstName").value;
+      const lastName = document.getElementById("lastName").value;
+      const email = document.getElementById("email").value;
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirmPassword").value;
+      const gender = document.getElementById("gender").value;
+
+      if (password !== confirmPassword) {
+        alert("Passwords do not match. Please try again.");
+        return;
+      }
+
+      // Get existing userData from local storage
+      let userDataJSON = localStorage.getItem("userData");
+      let userData = [];
+
+      if (userDataJSON) {
+        userData = JSON.parse(userDataJSON);
+      }
+
+      // Check if the user with the same username already exists
+      const existingUser = userData.find(user => user.username === username);
+
+      if (existingUser) {
+        alert("User with this username already exists. Please choose a different username.");
+        return;
+      }
+
+      // Generate a unique ID for the new user
+      const userId = generateUniqueId();
+
+      // Create a new user object
+      const user = {
+        id: userId,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        username: username,
+        password: password,
+        gender: gender,
+        isLogin: false,
+      };
+
+      // Push the new user object into the userData array
+      userData.push(user);
+
+      // Update the local storage with the modified userData array
+      localStorage.setItem("userData", JSON.stringify(userData));
+
+      alert("You have successfully registered.");
+      // After registration, redirect the user to the login page
+      window.location.href = "login.html";
+    });
+});
+
+// Function to generate a unique ID  
+function generateUniqueId() {
+  return Math.floor(Math.random() * 90000) + 10000;
+}
+
