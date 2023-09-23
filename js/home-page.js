00668f1fb2e8f5a41bc8b6d3b35ba930cc7ca06c
anand <rd.anandv@gmail.com>
2023-09-23 21:53:41 +0530

    user restricted to go other page after login functionality added

--- js/home-page.js
+++ js/home-page.js
@@ -1,7 +1,26 @@
-const home=document.querySelector('.home')
+const home = document.querySelector(".home");
 
-fetch('/home-page.html')
-.then((res)=>res.text())
-.then(data=>{
-    home.innerHTML=data;
+fetch("/home-page.html")
+  .then((res) => res.text())
+  .then((data) => {
+    home.innerHTML = data;
+  });
+
+//Check if user not logout redirect on user student home page
+
+window.addEventListener("load", function () {
+  const allUserDataJSON = localStorage.getItem("userData");
+
+  if (allUserDataJSON) {
+    const allUserData = JSON.parse(allUserDataJSON);
+
+    const loggedInUser = allUserData.find(
+      (userData) => userData.isLogin === true
+    );
+
+    if (loggedInUser) {
+      alert("You are already logged in!");
+      window.location.href = "/student-home-page.html";
+    }
+  }
 });
--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,13 @@
-setTimeout(() => {
+const logout = setInterval(() => {
+  var logoutButton = document.getElementById("logout");
+  if (logoutButton) {
+    userLogout();
+  }
+}, 1000);
+
+function userLogout() {
+  clearInterval(logout);
+
   const logoutButton = document.getElementById("logout");
 
   logoutButton.addEventListener("click", function (event) {
@@ -17,9 +26,12 @@ setTimeout(() => {
         loggedInUser.isLogin = false;
 
         localStorage.setItem("userData", JSON.stringify(allUserData));
-
+        alert("Are you sure want to logout!");
         window.location.href = "/login.html";
-      } 
-    } 
+      } else {
+        alert("You are already logout!");
+        window.location.href = "/login.html";
+      }
+    }
   });
-}, 50);
+}
